c141409bc7e8808af784ddac81eb4ea0668ccf94
nikolamarasovic <60428934+nikolamarasovic@users.noreply.github.com>
2022-05-25 19:41:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ make -f "${WORKSPACE}/Makefile"
 #cd "${WORKSPACE}"
 var="$(./main.exe)"
 echo $var
-
+if $var ~= "FAILED"; then
+  echo "Build is unstable"
+  exit 1
+fi
+echo "Build is stable"
+exit 0
